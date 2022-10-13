c52dd66ae04cbdfef0dc27e1ecc95f8dcb9d7611
Samuel Jansem <samuel.jansem@gmail.com>
2022-10-13 08:44:39 -0300

    ci: update config.base path

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
     },
     viteFinal: (config, { configType }) => {
         if (configType === 'PRODUCTION') {
-            config.base = '/react-design-system/';
+            config.base = '/react-design-system-sample/';
         }
 
         return config;
